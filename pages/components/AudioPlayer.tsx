1c559c9ab90bc0aa69e5ef17e57e64e16a1d5524
Muxthafa <musthafamohd0@gmail.com>
2024-08-12 17:07:50 +0530

    Add sw logs

--- pages/components/AudioPlayer.tsx
+++ pages/components/AudioPlayer.tsx
@@ -27,9 +27,26 @@
       transformSecondsToTimeString(0)
     );
 
+    useEffect(()=>{
+      if ("serviceWorker" in navigator) {
+        // Register a service worker hosted at the root of the
+        // site using the default scope.
+        navigator.serviceWorker.register("/sw.js").then(
+          (registration) => {
+            console.log("Service worker registration succeeded:", registration);
+          },
+          (error) => {
+            console.error(`Service worker registration failed: ${error}`);
+          },
+        );
+      } else {
+        console.error("Service workers are not supported.");
+      }
+    },[])
+    
+
     useEffect(() => {
         if (audioRef.current) audioRef.current?.play();
-    console.log('audioRef.current',audioRef.current);
     
         return () => {
           audioRef.current?.pause();
