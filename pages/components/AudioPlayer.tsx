1eb423c37d29d825e130e8655569196d61a48580
Muxthafa <musthafamohd0@gmail.com>
2024-08-12 14:12:15 +0530

    First commit

---
+++ pages/components/AudioPlayer.tsx
@@ -0,0 +1,110 @@
+
+  import { MutableRefObject, useEffect, useRef, useState } from "react";
+  import styles from "./AudioPlayer.module.scss";
+  
+  const AUDIO_PROGRESS_INDICATOR_MULTIPLIER = 10000;
+
+  const transformSecondsToTimeString = (seconds: number | undefined) => {
+    if (!seconds) return "0:00";
+  
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+  
+    return `${minutes.toString().padStart(1, "0")}:${remainingSeconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  const AudioPlayer = () => {
+    const audioRef: MutableRefObject<HTMLAudioElement | null> = useRef(null);
+  
+    const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+    const [audioCurrentTime, setAudioCurrentTime] = useState(0);
+    const [audioDurationString, setAudioDurationString] = useState(
+      transformSecondsToTimeString(0)
+    );
+    const [audioCurrentTimeString, setAudioCurrentTimeString] = useState(
+      transformSecondsToTimeString(0)
+    );
+
+    useEffect(() => {
+        if (audioRef.current) audioRef.current?.play();
+    console.log('audioRef.current',audioRef.current);
+    
+        return () => {
+          audioRef.current?.pause();
+        };
+      }, [audioRef]);
+
+  
+    const audioReadyHandler = () => {
+      setAudioCurrentTime(audioRef.current?.currentTime!);
+  
+      // Sometime actual audio duration is more than the recorder duration due to delay in recorder stopping the audio record
+      if (
+        isFinite(audioRef.current?.duration!) &&
+        audioRef.current?.duration!
+      ) {
+        const updatedDuration = Math.ceil(audioRef.current?.duration!);
+        setAudioDurationString(transformSecondsToTimeString(updatedDuration));
+      }
+    };
+  
+    const audioTimeUpdateHandler = () => {
+      setAudioCurrentTime(audioRef.current?.currentTime!);
+      setAudioCurrentTimeString(
+        transformSecondsToTimeString(audioRef.current?.currentTime)
+      );
+    };
+
+  
+    const audioSeekHandler = (seekTime: number) => {
+      if (audioRef.current?.currentTime) {
+        audioRef.current.currentTime = seekTime;
+      }
+    };
+  
+    return (
+      <div className={styles.audioPlayer}>
+        <div>
+          <audio
+            ref={audioRef}
+            src={'https://www2.cs.uic.edu/~i101/SoundFiles/StarWars60.wav'}
+            preload="metadata"
+            onLoadedMetadata={audioReadyHandler}
+            onTimeUpdate={audioTimeUpdateHandler}
+            onPlay={() => setIsAudioPlaying(true)}
+            onPause={() => setIsAudioPlaying(false)}
+          />
+          <input
+            type="range"
+            aria-hidden="true"
+            className={styles.seek}
+            onChange={(event) => {
+              audioSeekHandler(
+                Math.floor(
+                  +event.target.value / AUDIO_PROGRESS_INDICATOR_MULTIPLIER
+                )
+              );
+            }}
+            value={Math.floor(
+              audioCurrentTime * AUDIO_PROGRESS_INDICATOR_MULTIPLIER
+            )}
+            min="0"
+            max={
+              Math.floor(audioRef.current?.duration!) *
+                AUDIO_PROGRESS_INDICATOR_MULTIPLIER ||
+              AUDIO_PROGRESS_INDICATOR_MULTIPLIER
+            }
+          />
+          <div className={styles.sleekSlider}>
+            <span className="body-3">{audioCurrentTimeString}</span>
+            <span className="body-3">- {audioDurationString}</span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AudioPlayer;
+  
---
+++ public/sw.js
@@ -0,0 +1,293 @@
+if (!self.define) {
+  let e,
+    s = {};
+  const a = (a, n) => (
+    (a = new URL(a + ".js", n).href),
+    s[a] ||
+      new Promise((s) => {
+        if ("document" in self) {
+          const e = document.createElement("script");
+          (e.src = a), (e.onload = s), document.head.appendChild(e);
+        } else (e = a), importScripts(a), s();
+      }).then(() => {
+        let e = s[a];
+        if (!e) throw new Error(`Module ${a} didn’t register its module`);
+        return e;
+      })
+  );
+  self.define = (n, c) => {
+    const t =
+      e ||
+      ("document" in self ? document.currentScript.src : "") ||
+      location.href;
+    if (s[t]) return;
+    let i = {};
+    const r = (e) => a(e, t),
+      o = { module: { uri: t }, exports: i, require: r };
+    s[t] = Promise.all(n.map((e) => o[e] || r(e))).then((e) => (c(...e), i));
+  };
+}
+define(["./workbox-4754cb34"], function (e) {
+  "use strict";
+  importScripts(),
+    self.skipWaiting(),
+    e.clientsClaim(),
+    e.precacheAndRoute(
+      [
+        {
+          url: "/_next/static/FHqctUAEmPb5JBKUTXTzr/_buildManifest.js",
+          revision: "79050f6694a7887fdba9c0416c524b8e",
+        },
+        {
+          url: "/_next/static/FHqctUAEmPb5JBKUTXTzr/_ssgManifest.js",
+          revision: "b6652df95db52feb4daf4eca35380933",
+        },
+        {
+          url: "/_next/static/chunks/framework-ecc4130bc7a58a64.js",
+          revision: "ecc4130bc7a58a64",
+        },
+        {
+          url: "/_next/static/chunks/main-d45951c37c10b214.js",
+          revision: "d45951c37c10b214",
+        },
+        {
+          url: "/_next/static/chunks/pages/_app-a7e2d84213bc89e5.js",
+          revision: "a7e2d84213bc89e5",
+        },
+        {
+          url: "/_next/static/chunks/pages/_error-77823ddac6993d35.js",
+          revision: "77823ddac6993d35",
+        },
+        {
+          url: "/_next/static/chunks/pages/index-594318efcd269a87.js",
+          revision: "594318efcd269a87",
+        },
+        {
+          url: "/_next/static/chunks/polyfills-78c92fac7aa8fdd8.js",
+          revision: "79330112775102f91e1010318bae2bd3",
+        },
+        {
+          url: "/_next/static/chunks/webpack-8fa1640cc84ba8fe.js",
+          revision: "8fa1640cc84ba8fe",
+        },
+        {
+          url: "/_next/static/css/517502bba6d5d088.css",
+          revision: "517502bba6d5d088",
+        },
+        {
+          url: "/_next/static/css/876d048b5dab7c28.css",
+          revision: "876d048b5dab7c28",
+        },
+        {
+          url: "/_next/static/media/26a46d62cd723877-s.woff2",
+          revision: "befd9c0fdfa3d8a645d5f95717ed6420",
+        },
+        {
+          url: "/_next/static/media/55c55f0601d81cf3-s.woff2",
+          revision: "43828e14271c77b87e3ed582dbff9f74",
+        },
+        {
+          url: "/_next/static/media/581909926a08bbc8-s.woff2",
+          revision: "f0b86e7c24f455280b8df606b89af891",
+        },
+        {
+          url: "/_next/static/media/6d93bde91c0c2823-s.woff2",
+          revision: "621a07228c8ccbfd647918f1021b4868",
+        },
+        {
+          url: "/_next/static/media/97e0cb1ae144a2a9-s.woff2",
+          revision: "e360c61c5bd8d90639fd4503c829c2dc",
+        },
+        {
+          url: "/_next/static/media/a34f9d1faa5f3315-s.p.woff2",
+          revision: "d4fe31e6a2aebc06b8d6e558c9141119",
+        },
+        {
+          url: "/_next/static/media/df0a9ae256c0569c-s.woff2",
+          revision: "d54db44de5ccb18886ece2fda72bdfe0",
+        },
+        { url: "/favicon.ico", revision: "c30c7d42707a47a3f4591831641e50dc" },
+        { url: "/manifest.json", revision: "ebf08d845f93ed0cc744da9e8e538bc2" },
+        { url: "/next.svg", revision: "8e061864f388b47f33a1c3780831193e" },
+        { url: "/vercel.svg", revision: "61c6b19abff40ea7acd577be818f3976" },
+      ],
+      { ignoreURLParametersMatching: [] }
+    ),
+    e.cleanupOutdatedCaches(),
+    e.registerRoute(
+      "/",
+      new e.NetworkFirst({
+        cacheName: "start-url",
+        plugins: [
+          {
+            cacheWillUpdate: async ({
+              request: e,
+              response: s,
+              event: a,
+              state: n,
+            }) =>
+              s && "opaqueredirect" === s.type
+                ? new Response(s.body, {
+                    status: 200,
+                    statusText: "OK",
+                    headers: s.headers,
+                  })
+                : s,
+          },
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
+      new e.CacheFirst({
+        cacheName: "google-fonts-webfonts",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 31536e3 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "google-fonts-stylesheets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 604800 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-font-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 604800 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-image-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 64, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\/_next\/image\?url=.+$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "next-image",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 64, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:mp3|wav|ogg)$/i,
+      new e.CacheFirst({
+        cacheName: "static-audio-assets",
+        plugins: [
+          new e.RangeRequestsPlugin(),
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:mp4)$/i,
+      new e.CacheFirst({
+        cacheName: "static-video-assets",
+        plugins: [
+          new e.RangeRequestsPlugin(),
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:js)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-js-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:css|less)$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "static-style-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\/_next\/data\/.+\/.+\.json$/i,
+      new e.StaleWhileRevalidate({
+        cacheName: "next-data",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      /\.(?:json|xml|csv)$/i,
+      new e.NetworkFirst({
+        cacheName: "static-data-assets",
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      ({ url: e }) => {
+        if (!(self.origin === e.origin)) return !1;
+        const s = e.pathname;
+        return !s.startsWith("/api/auth/") && !!s.startsWith("/api/");
+      },
+      new e.NetworkFirst({
+        cacheName: "apis",
+        networkTimeoutSeconds: 10,
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 16, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      ({ url: e }) => {
+        if (!(self.origin === e.origin)) return !1;
+        return !e.pathname.startsWith("/api/");
+      },
+      new e.NetworkFirst({
+        cacheName: "others",
+        networkTimeoutSeconds: 10,
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
+        ],
+      }),
+      "GET"
+    ),
+    e.registerRoute(
+      ({ url: e }) => !(self.origin === e.origin),
+      new e.NetworkFirst({
+        cacheName: "cross-origin",
+        networkTimeoutSeconds: 10,
+        plugins: [
+          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 3600 }),
+        ],
+      }),
+      "GET"
+    );
+});
